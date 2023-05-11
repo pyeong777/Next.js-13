32488de8faa053b20c7e6a7c6409607fcd14292a
pyeong777 <sdp1123@naver.com>
2023-05-11 17:22:20 +0900

    fetch CSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,20 +1,13 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
-import styles from "./page.module.css";
-import { revalidate } from "./[slug]/page";
+import MeowArticle from "@/components/MeowArticle";
 
 // export const revalidate = 3;
 
 export default async function productPage() {
   // 서버 파일(db)에 있는 제품의 리스트를 읽어와서 보여준다
   const products = await getProducts();
-  const res = await fetch("https://meowfacts.herokuapp.com", {
-    next: { revalidate: 0 },
-    //revalidate 값 0이면 ssr 그이상이면 isr
-    // cache: "no-store", //no store도 ssr
-  });
-  const data = await res.json();
-  const factText = data.data[0];
+
   return (
     <>
       <h1>제품 소개 페이지</h1>
@@ -25,7 +18,7 @@ export default async function productPage() {
           </li>
         ))}
       </ul>
-      <article className={styles.article}>{factText}</article>
+      <MeowArticle />
     </>
   );
 }
