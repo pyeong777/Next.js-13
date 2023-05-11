fb09a3a3be3e38ef9385451b8058357547351585
pyeong777 <sdp1123@naver.com>
2023-05-11 19:06:35 +0900

    Image

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getProduct, getProducts } from "@/service/products";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export const revalidate = 3;
@@ -21,7 +22,17 @@ export default async function productDetailPage({ params: { slug } }: Props) {
     notFound();
   }
   //서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product.name}제품 설명</h1>;
+  return (
+    <>
+      <h1>{product.name}제품 설명</h1>
+      <Image
+        src={`/images/${product.image}`}
+        alt={product.name}
+        width="300"
+        height="300"
+      />
+    </>
+  );
 }
 
 export async function generateStaticParams() {
