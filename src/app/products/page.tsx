9f1bb35fcb5a665f1ace1f3e079de3759567265a
pyeong777 <sdp1123@naver.com>
2023-05-11 14:11:35 +0900

    ISR 렌더링

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,8 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 
+export const revalidate = 3;
+
 export default async function productPage() {
   // 서버 파일(db)에 있는 제품의 리스트를 읽어와서 보여준다
   const products = await getProducts();
